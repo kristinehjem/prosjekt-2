31fa5fd4c331d6405f8e014e4ee90efe0caa8205
Jørgen Usterud Myrvold <jorgeum@stud.ntnu.no>
2021-09-13 11:44:58 +0200

    Started styling header
    Co-authored-by: bmksimons <bmksimons@users.noreply.github.com>

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,3 +1,14 @@
-export default function Header() {
-  return <h1>This is Header</h1>;
+import React, { Component } from "react";
+import logo from "./assets/gitlab-logo.svg";
+import "./Header.css";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div id="header-container">
+        <img id="header-logo" src={logo} alt="Logo" />
+        <h1 id="header-text">This is Header</h1>
+      </div>
+    );
+  }
 }
