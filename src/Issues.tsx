6124a03fc56617f1fd63725e95835066b1b0b6a8
krishjem <10qg5423LD>
2021-09-13 12:36:18 +0200

    create grid for issue component resolves #2

--- src/Issues.tsx
+++ src/Issues.tsx
@@ -1,5 +1,25 @@
 import { GITLAB_TOKEN } from "./tokens";
+import "./Issues.css";
+import React, { MouseEvent } from 'react';
 
 export default function Issues() {
-  return <h1>This is Issues</h1>;
+  let issues: string[] = ["issue 1sssssssssss sssssssssssssss", "issue 2", "issue 3", "issue 4", "issue 5", "issue 6", "issue 7"]
+  let issueItems = issues.map((issue) =>
+  <div className = "issue" onClick = {showIssueDescription}>{issue}</div>
+  );
+  return (
+    <div className = "wrapper">
+      <h1>Issues</h1>
+      <div className = "issueList">
+        {issueItems}
+      </div>
+    </div>
+    
+  );
+  
+function showIssueDescription(event: MouseEvent<HTMLDivElement>) {
+  console.log(event);
+  
 }
+}
+
