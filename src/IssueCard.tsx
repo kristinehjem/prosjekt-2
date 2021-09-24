ae798ff5ab70dbbd9647ad35a78db103239481d2
krishjem <10qg5423LD>
2021-09-24 09:07:26 +0200

    #16 Added styling for issuecards

--- src/IssueCard.tsx
+++ src/IssueCard.tsx
@@ -6,23 +6,29 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import "./IssueCard.css"
+import { fontWeight } from '@mui/system';
 
-
+//code from https://mui.com/components/cards/
 
 export default function IssueCard(props: {title: string, description: string, issueNumber: number, labels: string[]}) {
+    let issueLabels = props.labels.map((label)=>
+    <div className="label">{label}</div>)
+    
+
   return (
-    <Card sx={{ width: 150, height: 200 }}>
+    <Card className ="issueCard" sx={{ width: 150, height: 200 }}>
       <CardContent>
-        <Typography sx={{ fontSize: 12 }} color="text.secondary" gutterBottom>
+        <Typography sx={{ fontSize: 12}} color="text.secondary" gutterBottom>
           #{props.issueNumber}
         </Typography>
-        <Typography sx={{ fontSize: 16 }} component="div">
+        <Typography sx={{ fontSize: 16, fontWeight: 'medium'}} component="div">
           {props.title}
         </Typography>
-        <Typography className="labels" sx={{ mb: 1.5 }} color="text.secondary">
-          {props.labels}
+        <Typography className="labels" sx={{ mb: 1.5 }} color="text.secondary" component="div">
+          {issueLabels}
         </Typography>
       </CardContent>
+      
     </Card>
   );
 }
